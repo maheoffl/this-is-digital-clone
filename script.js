2449b9eae55e33928641a5486b5ab0125886d441
Maheoffl <chmahesh997@gmail.com>
2023-09-06 08:38:03 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+gsap.from(".nav h1", {
+    opacity: 0,
+    delay: 0.5,
+    duration: 2,
+    
+})
+gsap.from(".limg ", {
+    opacity: 0,
+    delay: 0.5,
+    duration: 1.4,
+    y:60
+    
+})
+gsap.from(".rimg ", {
+    opacity: 0,
+    delay: 0.5,
+    duration:1.4,
+    x:60
+    
+})
+gsap.from(".rimgbottom  ", {
+    opacity: 0,
+    delay: 0.5,
+    duration:1.4,
+    y:-60
+    
+})
